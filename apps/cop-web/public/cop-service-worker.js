eb00a89221b24f99459d059184455c4a293b23c3
voldzi <jiri.volek@voldzi.cz>
2026-07-12 21:51:43 +0200

    fix(chat): retain current auth on refresh failure

--- apps/cop-web/public/cop-service-worker.js
+++ apps/cop-web/public/cop-service-worker.js
@@ -1,4 +1,4 @@
-const COP_SW_VERSION = "cop-pwa-offline-20260712-5";
+const COP_SW_VERSION = "cop-pwa-offline-20260712-6";
 const APP_SHELL_CACHE = `${COP_SW_VERSION}:shell`;
 const RUNTIME_CACHE = `${COP_SW_VERSION}:runtime`;
 const TILE_CACHE = `${COP_SW_VERSION}:tiles`;
