6f80ee28830fcb0eb2bb4a6b28e9837670ffaeba
voldzi <jiri.volek@voldzi.cz>
2026-09-19 17:35:02 +0200

    fix(map): lower hydrology overview threshold

--- apps/cop-web/src/CopMap.tsx
+++ apps/cop-web/src/CopMap.tsx
@@ -3188,7 +3188,7 @@ function CopMapComponent({
           id: situationHydroReferenceIconLayerId,
           type: "symbol",
           source: situationSourceId,
-          minzoom: 6,
+          minzoom: 4,
           maxzoom: 12.2,
           filter: [
             "all",
